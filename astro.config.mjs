72a3b2f3f3c3dfbf28455688ca872d6d9687be32
LeonardM <martinisleo@gmail.com>
2024-03-10 12:57:24 +0100

    feat/RSS feed and checkout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-bootcamp-leonardm01.netlify.com",
+  redirects: {
+    "/rss": "/rss.xml",
+  },
   server: {
     port: 3000,
   },
+  integrations: [react(), solidJs()],
 });
--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,6 +3,12 @@ import type { CartItem, ShopItem } from "../types/types";
 
 export const $cart = map<Record<number, CartItem>>({});
 
+/**
+ * Adds an item to the cart.
+ * If the item already exists in the cart, the quantity is incremented by 1.
+ * If the item does not exist in the cart, a new entry is created with a quantity of 1.
+ * @param item - The item to be added to the cart.
+ */
 export function addItemToCart(item: ShopItem) {
   const cartItem = $cart.get()[item.id];
   const quantity = cartItem ? cartItem.quantity + 1 : 1;
@@ -13,12 +19,21 @@ export function addItemToCart(item: ShopItem) {
   });
 }
 
+/**
+ * Removes an item from the cart.
+ * @param itemId - The ID of the item to be removed.
+ */
 export function removeItemFromCart(itemId: number) {
   // @ts-ignore
   $cart.setKey(itemId, undefined);
 }
 
-export const subtotal = computed($cart, (entries) => {
+/**
+ * Calculates the subtotal of the cart based on the entries.
+ * @param entries - The entries in the cart.
+ * @returns The subtotal of the cart.
+ */
+export const $subtotal = computed($cart, (entries) => {
   let subtotal = 0;
 
   Object.values(entries).forEach((entry) => {
