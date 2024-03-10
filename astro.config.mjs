6522b4c7ad21ffa973c9451072b4d9e884faa34d
LeonardM <martinisleo@gmail.com>
2024-03-11 00:46:05 +0100

    feat/updated domain in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-bootcamp-leonardm01.netlify.com',
+  site: 'http://sndwch-astro.leonard.solutions/',
   redirects: {
     '/rss': '/rss.xml',
   },
