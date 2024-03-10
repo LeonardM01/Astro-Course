d843cd4d200bf347fb3f3988d5551ae41f227860
LeonardM <martinisleo@gmail.com>
2024-03-11 00:33:17 +0100

    fix/deployment issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import solidJs from '@astrojs/solid-js';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-bootcamp-leonardm01.netlify.com",
+  site: 'https://astro-bootcamp-leonardm01.netlify.com',
   redirects: {
-    "/rss": "/rss.xml",
+    '/rss': '/rss.xml',
   },
   server: {
     port: 3000,
   },
   integrations: [react(), solidJs()],
-  output: "hybrid",
+  output: 'server',
+  adapter: netlify(),
 });
