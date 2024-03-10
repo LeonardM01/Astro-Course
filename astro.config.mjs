cd8d4982114b591eea71aab28c811782ca5078fa
LeonardM <martinisleo@gmail.com>
2024-03-11 00:22:24 +0100

    feat/newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
@@ -13,4 +12,5 @@ export default defineConfig({
     port: 3000,
   },
   integrations: [react(), solidJs()],
+  output: "hybrid",
 });
